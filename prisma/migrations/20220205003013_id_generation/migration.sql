9e6987b534a07c3b900fbd5c362ff2b86f6991f9
minna <yakubuaminat94@gmail.com>
2022-02-06 00:00:07 +0100

    feat: new migrations

---
+++ prisma/migrations/20220205003013_id_generation/migration.sql
@@ -0,0 +1,108 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT E'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resources" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "tag" TEXT NOT NULL,
+    "imageUrl" TEXT,
+
+    CONSTRAINT "Resources_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SuggestedResources" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "tag" TEXT NOT NULL,
+
+    CONSTRAINT "SuggestedResources_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" SERIAL NOT NULL,
+    "compound_id" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "provider_type" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "access_token_expires" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ResourcesToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_compound_id_key" ON "accounts"("compound_id");
+
+-- CreateIndex
+CREATE INDEX "providerAccountId" ON "accounts"("provider_account_id");
+
+-- CreateIndex
+CREATE INDEX "providerId" ON "accounts"("provider_id");
+
+-- CreateIndex
+CREATE INDEX "userId" ON "accounts"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_session_token_key" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_access_token_key" ON "sessions"("access_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ResourcesToUser_AB_unique" ON "_ResourcesToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ResourcesToUser_B_index" ON "_ResourcesToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ResourcesToUser" ADD FOREIGN KEY ("A") REFERENCES "Resources"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ResourcesToUser" ADD FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
