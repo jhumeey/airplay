86dcd0b1cc58329f4718aead662e72c11f0ea7dd
minna <yakubuaminat94@gmail.com>
2022-08-15 18:13:56 +0100

    feat: navbar structure

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,14 +1,20 @@
-import{ GetStaticProps } from 'next';
-import Head from 'next/head'
-import * as React from 'react';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import * as React from "react";
 
-import prisma from '../../lib/prisma';
-import ResourceModal from '../modals/SuggestResourceModal';
-import DashMobileNavbar from '../navbar/DashMobileNavbar';
-import DashTopBar from '../navbar/DashTopBar';
-import Sidebar from '../navbar/Sidebar';
+import prisma from "../../lib/prisma";
+import ResourceModal from "../modals/SuggestResourceModal";
+import DashMobileNavbar from "../navbar/DashMobileNavbar";
+import DashTopBar from "../navbar/DashTopBar";
+import Sidebar from "../navbar/Sidebar";
 
-function Layout({ children, pageTitle, description, previewImage, siteName }: any) {
+function Layout({
+  children,
+  pageTitle,
+  description,
+  previewImage,
+  siteName,
+}: any) {
   const [toggleNav, setToogleNavBar] = React.useState(false);
   const [showModal, setShowModal] = React.useState(false);
 
@@ -17,29 +23,231 @@ function Layout({ children, pageTitle, description, previewImage, siteName }: an
   };
 
   return (
-    <>
+    <div className="a-p-container">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="description" content={description} />
         <meta property="og:title" content={pageTitle} key="ogtitle" />
-       <meta property="og:description" content={description} key="ogdesc" />
-       <meta property="og:image" content={previewImage} key="ogimage" />
-       <meta property="og:site_name" content={siteName} key="ogsitename" />
-       <title>{pageTitle}</title>
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <title>{pageTitle}</title>
       </Head>
-    <main className="flex flex-col min-h-screen text-base subpixel-antialiased font-normal bg-black-brand-09 lg:flex-row">
-      <DashMobileNavbar toggleNavBar={toggleNavBar} />
-      <Sidebar toggleState={toggleNav} toggleNavBar={toggleNavBar} />
-      <div className="flex flex-col flex-1 px-4 py-4 overflow-hidden lg:py-8 lg:px-6 xl:px-8">
-        <DashTopBar setShowModal={setShowModal} />
-        <ResourceModal showModal={showModal} setShowModal={setShowModal} />
-        <div className="flex-1 py-4 lg:py-10">
-          {children}
+
+      <main className="flex flex-col min-h-screen text-base subpixel-antialiased font-normal">
+        <div className="bg-transparent">
+          <header className="py-4 md:py-6">
+            <div className="container px-4 mx-auto sm:px-6 lg:px-8">
+              <div className="flex items-center justify-between">
+                <div className="flex-shrink-0">
+                  <a
+                    href="#"
+                    title=""
+                    className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    <img
+                      className="w-auto h-8"
+                      src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg"
+                      alt=""
+                    />
+                  </a>
+                </div>
+
+                <div className="flex lg:hidden">
+                  <button type="button" className="text-gray-900">
+                    <svg
+                      className="w-7 h-7"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="1.5"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+
+                <div className="hidden lg:flex lg:ml-10 xl:ml-16 lg:items-center lg:justify-center lg:space-x-8 xl:space-x-16">
+                  <a
+                    href="#"
+                    title=""
+                    className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Solutions{" "}
+                  </a>
+
+                  <a
+                    href="#"
+                    title=""
+                    className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Industries{" "}
+                  </a>
+
+                  <a
+                    href="#"
+                    title=""
+                    className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Fees{" "}
+                  </a>
+
+                  <a
+                    href="#"
+                    title=""
+                    className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    About Rareblocks{" "}
+                  </a>
+                </div>
+
+                <div className="hidden lg:ml-auto lg:flex lg:items-center lg:space-x-8 xl:space-x-10">
+                  <a
+                    href="#"
+                    title=""
+                    className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                  >
+                    {" "}
+                    Sign in{" "}
+                  </a>
+
+                  <DashTopBar setShowModal={setShowModal} />
+                </div>
+              </div>
+            </div>
+          </header>
+
+          <section className="pt-12 pb-12 sm:pb-16 lg:pt-8">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-16">
+                <div>
+                  <div className="text-center lg:text-left">
+                    <h1 className="text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj">
+                      A special credit card made for Developers.
+                    </h1>
+                    <p className="mt-2 text-lg text-gray-600 sm:mt-8 font-inter">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Vehicula massa in enim luctus. Rutrum arcu.
+                    </p>
+
+                    <form action="#" method="POST" className="mt-8 sm:mt-10">
+                      <div className="relative p-2 sm:border sm:border-gray-400 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
+                        <input
+                          type="email"
+                          name=""
+                          id=""
+                          placeholder="Enter email address"
+                          className="block w-full px-4 py-4 text-gray-900 placeholder-gray-900 bg-transparent border border-gray-400 outline-none focus:border-gray-900 focus:ring-1 focus:ring-gray-900 rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
+                          required=""
+                        />
+                        <div className="mt-4 sm:mt-0 sm:absolute sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
+                          <button
+                            type="submit"
+                            className="inline-flex px-6 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-lg focus:outline-none focus:bg-gray-600 font-pj hover:bg-gray-600"
+                          >
+                            Get Free Card
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+
+                  <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
+                    <div className="flex items-center">
+                      <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">
+                        2943
+                      </p>
+                      <p className="ml-3 text-sm text-gray-900 font-pj">
+                        Cards
+                        <br />
+                        Delivered
+                      </p>
+                    </div>
+
+                    <div className="hidden sm:block">
+                      <svg
+                        className="text-gray-400"
+                        width="16"
+                        height="39"
+                        viewBox="0 0 16 39"
+                        fill="none"
+                        stroke="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <line
+                          x1="0.72265"
+                          y1="10.584"
+                          x2="15.7226"
+                          y2="0.583975"
+                        ></line>
+                        <line
+                          x1="0.72265"
+                          y1="17.584"
+                          x2="15.7226"
+                          y2="7.58398"
+                        ></line>
+                        <line
+                          x1="0.72265"
+                          y1="24.584"
+                          x2="15.7226"
+                          y2="14.584"
+                        ></line>
+                        <line
+                          x1="0.72265"
+                          y1="31.584"
+                          x2="15.7226"
+                          y2="21.584"
+                        ></line>
+                        <line
+                          x1="0.72265"
+                          y1="38.584"
+                          x2="15.7226"
+                          y2="28.584"
+                        ></line>
+                      </svg>
+                    </div>
+
+                    <div className="flex items-center">
+                      <p className="text-3xl font-medium text-gray-900 sm:text-4xl font-pj">
+                        $1M+
+                      </p>
+                      <p className="ml-3 text-sm text-gray-900 font-pj">
+                        Transaction
+                        <br />
+                        Completed
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <img
+                    className="w-full"
+                    src="https://d33wubrfki0l68.cloudfront.net/d6f1462500f7670e0db6b76b35054a081679a5a0/0ce15/images/hero/5.1/illustration.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
-    </main>
-    </>
+        <DashMobileNavbar toggleNavBar={toggleNavBar} />
+        <div className="flex flex-col flex-1 px-4 py-4 overflow-hidden lg:py-8 lg:px-6 xl:px-8">
+          <ResourceModal showModal={showModal} setShowModal={setShowModal} />
+          <div className="flex-1 py-4 lg:py-10">{children}</div>
+        </div>
+      </main>
+    </div>
   );
 }
 export default Layout;
--- components/navbar/DashTopBar.tsx
+++ components/navbar/DashTopBar.tsx
@@ -3,10 +3,7 @@ import { ToggleModalProps } from "./types";
 
 export default function DashTopBar(props: ToggleModalProps) {
   return (
-    <div className="flex justify-between items-center w-full mx-auto max-w-screen-2xl p-2">
-      <div className="hidden md:block">
-        <h1 className="mb-1 text-2xl font-bold text-white">Resources</h1>
-      </div>
+    <div>
       <div className="flex justify-between flex-1 space-x-4 md:justify-end">
         <button onClick={() => props.setShowModal(true)} className="inline-flex border border-gray-brand-03 text-green-brand-01 py-3 px-6 focus:outline-none rounded-lg text-lg hover:animate-bounce hover:text-white">Suggest a resource</button>
       </div>
